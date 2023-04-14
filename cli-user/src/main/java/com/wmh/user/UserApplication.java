7a292a6f1b1369a763b5d2f093eb77652e140f60
吴铭浩 <itbwmh@163.com>
2023-04-14 11:24:38 +0800

    添加启动器代码

---
+++ cli-user/src/main/java/com/wmh/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.wmh.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wmh
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+}
