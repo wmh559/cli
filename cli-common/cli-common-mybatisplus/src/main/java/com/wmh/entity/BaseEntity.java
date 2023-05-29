14f5489cffd8cd1945f09bfdfbfc5d241c52be22
吴铭浩 <itbwmh@163.com>
2023-05-29 19:57:18 +0800

    提交实体优化代码

---
+++ cli-common/cli-common-mybatisplus/src/main/java/com/wmh/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.wmh.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author wmh
+ */
+@Data
+public class BaseEntity {
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updateBy;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Integer deleteFlag;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Integer version;
+}
--- cli-user/src/main/java/com/wmh/user/entity/po/UserPo.java
+++ cli-user/src/main/java/com/wmh/user/entity/po/UserPo.java
@@ -1,9 +1,9 @@
 package com.wmh.user.entity.po;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.wmh.entity.BaseEntity;
 import lombok.Data;
 
-import java.util.Date;
 
 /**
  * @author wmh
@@ -11,7 +11,7 @@ import java.util.Date;
  */
 @TableName("cli_user")
 @Data
-public class UserPo {
+public class UserPo extends BaseEntity {
 
     @TableId(value = "id",type = IdType.AUTO)
     private Long id;
@@ -19,22 +19,4 @@ public class UserPo {
     private String name;
 
     private Integer age;
-
-    @TableField(fill = FieldFill.INSERT)
-    private String createBy;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private String updateBy;
-
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Integer deleteFlag;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Integer version;
 }
