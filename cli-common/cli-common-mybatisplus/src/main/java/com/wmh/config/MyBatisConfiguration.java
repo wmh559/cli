23688e2109b53bf57160429fcd09af36889fca30
吴铭浩 <itbwmh@163.com>
2023-05-29 19:50:08 +0800

    配置sql监控为可开启

--- cli-common/cli-common-mybatisplus/src/main/java/com/wmh/config/MyBatisConfiguration.java
+++ cli-common/cli-common-mybatisplus/src/main/java/com/wmh/config/MyBatisConfiguration.java
@@ -1,6 +1,7 @@
 package com.wmh.config;
 
 import com.wmh.inteceptor.SqlBeautyInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class MyBatisConfiguration {
 
     @Bean
+    //设置sql打印，可以在配置文件配置sqlInteceptor.enabled控制
+    @ConditionalOnProperty(name = {"sqlInteceptor.enabled"},havingValue = "true", matchIfMissing = true)
     public SqlBeautyInterceptor sqlBeautyInterceptor() {
         return new SqlBeautyInterceptor();
     }
