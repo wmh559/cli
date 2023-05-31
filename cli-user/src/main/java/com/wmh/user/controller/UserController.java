62d7329983097f6e01f4aae08b49bd17bc48e3b8
吴铭浩 <itbwmh@163.com>
2023-05-31 08:29:55 +0800

    整合redis和swagger

--- cli-user/src/main/java/com/wmh/user/controller/UserController.java
+++ cli-user/src/main/java/com/wmh/user/controller/UserController.java
@@ -7,7 +7,10 @@ import com.wmh.user.entity.req.UserReq;
 import com.wmh.user.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * @author wmh
@@ -18,6 +21,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @PostMapping("/addUser")
     public Result addUser(@RequestBody UserReq userReq) {
@@ -33,5 +39,11 @@ public class UserController {
     public Result deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return Result.ok();
+    }
+
+    @GetMapping("/test")
+    public void TestRedis() {
+        redisTemplate.opsForValue().set("test", "test");
+        System.out.println(redisTemplate.opsForValue().get("test"));
     }
 }
